12b1fca67111835502050084471ee772a40864dd
Enenche Simon <enenchesimon@gmail.com>
2023-07-13 13:53:17 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 /*
-*main - Entry Point
+* main - Entry Point
 *
-* Return: Always 0(Succes)
+* Return: Always 0 (Succes)
 */
-int main(void) {
+int main(void) 
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
