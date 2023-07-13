0b43bed408bf69abf8881771d8fa759e2e0b3ed0
Enenche Simon <enenchesimon@gmail.com>
2023-07-13 14:52:50 -0700

    Size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+* main - A program thst prints size
+*
+* Return 0 (Success)
+*/
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long int d;
+	float f;
+	
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)size of (a));
+	printf("Size of an int: %lu bytes(s)\n", (unsigned long)size of (b));
+	printf("Size of a long int: %lu bytes(s)\n", (unsigned long)size of (c));
+	printf("Size of a long int: %lu bytes(s)\n", (unsigned long)size of (d));
+	printf("Size of a float: %lu bytes(s)\n", (unsigned long)size of (f));
+	return (0);
+}
